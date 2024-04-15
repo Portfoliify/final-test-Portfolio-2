16c4b4f570b9624cea75cf4130523d18577f0291
Portfoliify <tnmadgaming9920@gmail.com>
2024-04-15 20:58:46 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -20,11 +20,11 @@ const App = () => {
   //Content
 
       const tagline = 'Available for freelance work';
-      const name = 'example';
+      const name = 'teena free';
       const email = 'sample@gmail.com';
       const heroLine1 = 'independent';
-      const heroLine2 = 'full-stack';
-      const heroLine3 = 'web developer';
+      const heroLine2 = 'student';
+      const heroLine3 = 'at srm';
       const gitHub = 'https://github.com';
       const linkedIn = 'https://www.linkedin.com';
       const twitter = 'https://twitter.com';
@@ -34,7 +34,7 @@ const App = () => {
       const project4 = ['AI snake', 'https://example.com', null, 'pic4.png'];
       const project5 = ['Personal Portfolio', 'https://example.com/', null, 'pic5.png'];
       const project6 = ['Movie finder', 'https://example.com/', null, 'pic6.png'];
-      const aboutLine1= "Hey! i'm example";
+      const aboutLine1= "Hey! i'm Mohan";
       const aboutLine2= 'a nineteen years old';
       const aboutLine3= 'ful-stack developer. Currently';
       const aboutLine4= 'based in Chennai, India';
